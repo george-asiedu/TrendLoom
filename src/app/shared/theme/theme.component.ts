3395b54a8c4f634d4710a3d43bbabcd4e07679eb
george-asiedu <asiedug41@gmail.com>
2025-07-11 15:07:53 +0000

    theme component updated to use computed signals for reactivity

--- src/app/shared/theme/theme.component.ts
+++ src/app/shared/theme/theme.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, computed } from '@angular/core';
 import { useThemeSwitcher } from '../utils/theme.utils';
 import { NgOptimizedImage } from '@angular/common';
 import { images } from '../utils/constants';
@@ -8,23 +8,18 @@ import { images } from '../utils/constants';
   standalone: true,
   imports: [NgOptimizedImage],
   templateUrl: './theme.component.html',
-  styleUrl: './theme.component.css'
+  styleUrl: './theme.component.css',
 })
-export class ThemeComponent implements OnInit {
-  private readonly storage = useThemeSwitcher('theme', 'light');
-  public isDark: boolean = this.storage.value() === 'dark';
+export class ThemeComponent {
+  private readonly themeStorage = useThemeSwitcher('theme', 'light');
+
+  public isDark = computed(() => this.themeStorage.value() === 'dark');
+  public currentTheme = computed(() => this.themeStorage.value());
+
   public moonIcon = images.moon;
   public sunIcon = images.sun;
-  public theme = this.isDark ? 'dark' : 'light';
-  
-  public ngOnInit(): void {
-    this.isDark = this.storage.value() === 'dark';
-  }
 
   public themeToggle(): void {
-    const newTheme = this.isDark ? 'light' : 'dark';
-    this.storage.value.set(newTheme);
-    this.isDark = newTheme === 'dark';
-    document.body.setAttribute('data-theme', newTheme);
+    this.themeStorage.toggle();
   }
 }
