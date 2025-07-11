dbdf2c83adfb31d688236ac5b62f7ba9c0a8c922
george-asiedu <asiedug41@gmail.com>
2025-07-11 15:06:51 +0000

    theme component imported and used

--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -2,13 +2,14 @@ import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { appRoutes, authRoutes, images } from '../utils/constants';
 import { NgOptimizedImage } from '@angular/common';
+import { ThemeComponent } from '../theme/theme.component';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [RouterModule, NgOptimizedImage],
+  imports: [RouterModule, NgOptimizedImage, ThemeComponent],
   templateUrl: './header.component.html',
-  styleUrl: './header.component.css'
+  styleUrl: './header.component.css',
 })
 export class HeaderComponent {
   public cartRoute = appRoutes.cart;
