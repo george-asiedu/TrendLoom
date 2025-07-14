b005b2b50d10e43b42b61768fa6b51d0c7669279
george-asiedu <asiedug41@gmail.com>
2025-07-14 15:32:14 +0000

    component routes paths and logo images property defined

---
+++ src/app/shared/footer/footer.component.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { appRoutes, images } from '../utils/constants';
+import { NgOptimizedImage } from '@angular/common';
+
+@Component({
+  selector: 'app-footer',
+  standalone: true,
+  imports: [RouterLink, NgOptimizedImage],
+  templateUrl: './footer.component.html',
+  styleUrl: './footer.component.css',
+})
+export class FooterComponent {
+  public logoImage = images.darkLogo;
+  public profileRoute = appRoutes.profile;
+  public aboutRoute = appRoutes.about;
+  public contactRoute = appRoutes.contact;
+  public productsRoute = appRoutes.products;
+  public categoriesRoute = appRoutes.categories;
+  public blogRoute = appRoutes.blog;
+}
