59a5f37181c78f6a8e815e9e23ea28bd558a8e6d
george-asiedu <asiedug41@gmail.com>
2025-07-14 21:10:48 +0000

    password accessibility implemented to toggle show and hide password

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,15 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { RouterLink } from '@angular/router';
-import { authRoutes } from '../../shared/utils/constants';
+import { authRoutes, images } from '../../shared/utils/constants';
+import { NgOptimizedImage } from '@angular/common';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink, NgOptimizedImage],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.css'
+  styleUrl: './login.component.css',
 })
 export class LoginComponent {
   public forgotPasswordRoute = authRoutes.forgotPassword;
   public signupRoute = authRoutes.signup;
+  public showPassword = signal<boolean>(false);
+  public eyeOpen = images.eyeOpen;
+  public eyeClose = images.eyeClose;
+
+  public toggleShowPassword(): void {
+    this.showPassword.update(value => !value);
+  }
+
+  public onKeyDown(event: KeyboardEvent): void {
+    if (event.key === 'Enter' || event.key === ' ') {
+      event.preventDefault();
+      this.toggleShowPassword();
+    }
+  }
 }
--- src/app/auth/signup/signup.component.ts
+++ src/app/auth/signup/signup.component.ts
@@ -1,14 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { RouterLink } from '@angular/router';
-import { authRoutes } from '../../shared/utils/constants';
+import { authRoutes, images } from '../../shared/utils/constants';
+import { NgOptimizedImage } from '@angular/common';
 
 @Component({
   selector: 'app-signup',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink, NgOptimizedImage],
   templateUrl: './signup.component.html',
   styleUrl: './signup.component.css',
 })
 export class SignupComponent {
   public loginRoute = authRoutes.login;
+  public showPassword = signal<boolean>(false);
+  public eyeOpen = images.eyeOpen;
+  public eyeClose = images.eyeClose;
+
+  public toggleShowPassword(): void {
+    this.showPassword.update(value => !value);
+  }
+
+  public onKeyDown(event: KeyboardEvent): void {
+    if (event.key === 'Enter' || event.key === ' ') {
+      event.preventDefault();
+      this.toggleShowPassword();
+    }
+  }
 }
