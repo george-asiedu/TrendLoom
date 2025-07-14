b93d8e1884de5e860fc8c94f81b1d72ef2e3be45
george-asiedu <asiedug41@gmail.com>
2025-07-14 20:05:43 +0000

    login route lazy loaded

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,12 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home/home.component';
-import { pageTitles } from './shared/utils/constants';
+import { authRoutes, pageTitles } from './shared/utils/constants';
 
 export const routes: Routes = [
-  { path: '', component: HomeComponent, title: pageTitles.home }
+  { path: '', component: HomeComponent, title: pageTitles.home },
+  {
+    path: authRoutes.login,
+    loadComponent: () => import('./auth/login/login.component').then(c => c.LoginComponent),
+    title: pageTitles.login,
+  },
 ];
