d301e2c2b3095671a19ccf06b0fa13742e162549
george-asiedu <asiedug41@gmail.com>
2025-07-16 14:22:45 +0000

    category template image properties defined

---
+++ src/app/home/category/category.component.ts
@@ -0,0 +1,20 @@
+import { NgOptimizedImage } from '@angular/common';
+import { Component } from '@angular/core';
+import { appRoutes, images } from '../../shared/utils/constants';
+
+@Component({
+  selector: 'app-category',
+  standalone: true,
+  imports: [NgOptimizedImage],
+  templateUrl: './category.component.html',
+  styleUrl: './category.component.css'
+})
+export class CategoryComponent {
+  public electronicsImage = images.ultra;
+  public fitnessImage = images.fitness;
+  public homeImage = images.home;
+  public cannonImage = images.cannon;
+  public furnitureImage = images.furniture;
+  public backpackImage = images.backpack;
+  public categoriesRoute = appRoutes.categories;
+}
