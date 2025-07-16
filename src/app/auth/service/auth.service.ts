bb05b06eb0c766d528705e829e9cc76bdf72c808
george-asiedu <asiedug41@gmail.com>
2025-07-16 17:06:54 +0000

    observable return types added to service methods

--- src/app/auth/service/auth.service.ts
+++ src/app/auth/service/auth.service.ts
@@ -1,7 +1,8 @@
 import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
 import { environment } from '../../../environments/environment';
-import { Signin, SigninResponse, Signup, SignupResponse } from '../../model/auth';
+import { Signin, SigninResponse, Signup, SignupResponse, VerifyAccountResponse } from '../../model/auth';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
@@ -10,15 +11,15 @@ export class AuthService {
   private authUrl = `${environment.baseUrl}/auth`;
   private http = inject(HttpClient);
 
-  public signup(user: Signup) {
+  public signup(user: Signup): Observable<SignupResponse> {
     return this.http.post<SignupResponse>(`${this.authUrl}/signup`, user);
   }
 
-  public signin(user: Signin) {
+  public signin(user: Signin): Observable<SigninResponse> {
     return this.http.post<SigninResponse>(`${this.authUrl}/signin`, user);
   }
 
-  public verifyAccount(code: string) {
-    return this.http.post<SignupResponse>(`${this.authUrl}/verify-account`, code);
+  public verifyAccount(code: string): Observable<VerifyAccountResponse> {
+    return this.http.post<VerifyAccountResponse>(`${this.authUrl}/verify-account`, { code });
   }
 }
