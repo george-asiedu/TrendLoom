c0c1d8280216344d2e777b5f503ee09779c8f73d
george-asiedu <asiedug41@gmail.com>
2025-07-16 14:23:30 +0000

    category component added to home component

--- src/app/home/home/home.component.ts
+++ src/app/home/home/home.component.ts
@@ -1,13 +1,12 @@
 import { Component } from '@angular/core';
 import { DiscoverComponent } from '../discover/discover.component';
+import { CategoryComponent } from '../category/category.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [DiscoverComponent],
+  imports: [DiscoverComponent, CategoryComponent],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+  styleUrl: './home.component.css',
 })
-export class HomeComponent {
-
-}
+export class HomeComponent {}
