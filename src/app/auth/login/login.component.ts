a0ce10c7199704b7c201ac799db2411f758d3d1b
george-asiedu <asiedug41@gmail.com>
2025-08-05 08:02:48 +0000

    eye icons changed to use svg

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,21 +1,43 @@
-import { Component, signal } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
 import { RouterLink } from '@angular/router';
-import { authRoutes, images } from '../../shared/utils/constants';
-import { NgOptimizedImage } from '@angular/common';
+import { authRoutes } from '../../shared/utils/constants';
+import { SpinnerComponent } from '../../shared/spinner/spinner.component';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { AuthFacade } from '../../shared/facade/auth/auth.facade';
+import { emailValidator } from '../../shared/validators/email.validator';
+import { passwordValidator } from '../../shared/validators/password.validator';
+import { Signin } from '../../model/auth';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [RouterLink, NgOptimizedImage],
+  imports: [RouterLink, SpinnerComponent, ReactiveFormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css',
 })
 export class LoginComponent {
-  public forgotPasswordRoute = authRoutes.forgotPassword;
-  public signupRoute = authRoutes.signup;
-  public showPassword = signal<boolean>(false);
-  public eyeOpen = images.eyeOpen;
-  public eyeClose = images.eyeClose;
+  public readonly forgotPasswordRoute = authRoutes.forgotPassword;
+  public readonly signupRoute = authRoutes.signup;
+  public readonly showPassword = signal<boolean>(false);
+
+  private readonly fb = inject(FormBuilder);
+  public readonly signinForm: FormGroup;
+  private readonly authFacade = inject(AuthFacade);
+  public readonly isLoading = this.authFacade.isLoading();
+
+  public constructor() {
+    this.signinForm = this.fb.group({
+      email: ['', [Validators.required, emailValidator()]],
+      password: ['', [Validators.required, passwordValidator()]],
+    });
+  }
+
+  public onSigninSubmit() {
+    if (this.signinForm.invalid) return;
+
+    const user: Signin = this.signinForm.value;
+    this.authFacade.signin(user);
+  }
 
   public toggleShowPassword(): void {
     this.showPassword.update(value => !value);
@@ -26,5 +48,9 @@ export class LoginComponent {
       event.preventDefault();
       this.toggleShowPassword();
     }
+  }
+
+  public getControl(value: string) {
+    return this.signinForm.get(value);
   }
 }
