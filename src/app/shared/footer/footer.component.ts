dd3737baef73c45bd5cefa0f1a19c01415ca2f2c
george-asiedu <asiedug41@gmail.com>
2026-02-16 13:13:33 +0000

    fixed image

--- src/app/shared/footer/footer.component.ts
+++ src/app/shared/footer/footer.component.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { appRoutes, images } from '../utils/constants';
-import { NgOptimizedImage } from '@angular/common';
 
 @Component({
   selector: 'app-footer',
   standalone: true,
-  imports: [RouterLink, NgOptimizedImage],
+  imports: [RouterLink],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.css',
 })
